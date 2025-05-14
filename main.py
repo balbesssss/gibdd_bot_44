2e6a2af3e272789ab5f0a3c21057e4835096984f
Максим Еперин <maksimeperin@gmail.com>
2025-05-14 15:47:37 +0300

    modified:   databes/models.py 	modified:   main.py 	modified:   requirements.txt

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import asyncio
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
-from bot.handlers.handlers import function
 
 load_dotenv(".env")
 bot = os.getenv('Bot')
@@ -14,7 +13,6 @@ dp = Dispatcher()
 async def main():
     """Запуск бота"""
     try:
-        function(dp)
         await dp.start_polling(bot, skip_updatet=True)
     finally:
         await bot.session.close()
