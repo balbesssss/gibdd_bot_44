067bf0a15b1493543390cb621df88a76fd2681d6
Bhdusuhs <147725400+Bhdusuhs@users.noreply.github.com>
2025-05-15 16:37:45 +0300

    Фильтр для обработчиков для роли Инспектор

--- filters/admin.py
+++ filters/admin.py
@@ -3,6 +3,7 @@ from database.models import Role
 from filters.user import IsUser
 
 
+# pylint: disable=R0903
 class IsAdmin(IsUser):
     """Проверяет является ли пользователь Администратором"""
     role = Role.get(name="Администратор")
--- filters/inspector.py
+++ filters/inspector.py
@@ -3,6 +3,7 @@ from database.models import Role
 from filters.user import IsUser
 
 
+# pylint: disable=R0903
 class IsInspector(IsUser):
     """Проверяет является ли пользователь Инспектором"""
     role = Role.get(name="Инспектор")
