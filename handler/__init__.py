6427b505d6459800e0d5b398742654863ba8b4d0
Nata-alt-ops <nataliaklucnikova9@gmail.com>
2025-05-15 10:29:48 +0300

    Pylint

--- handler/__init__.py
+++ handler/__init__.py
@@ -1,9 +1,10 @@
 """подключение роутеров"""
 from aiogram import Dispatcher
-from start import router as start_router
+from .start import router as start_router
 
 
 def add_routers(dp: Dispatcher):
+    """Подключение роутеров"""
     dp.include_routers([
         start_router
     ])
