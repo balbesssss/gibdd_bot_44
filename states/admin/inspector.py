595df36062e92d555e3847bedbf16c91074f9c8f
Nata-alt-ops <nataliaklucnikova9@gmail.com>
2025-05-15 11:19:37 +0300

    # pylint: disable=R0903

--- states/admin/inspector.py
+++ states/admin/inspector.py
@@ -1,6 +1,6 @@
 """Состояние для добовления инспектора"""
 from aiogram.fsm.state import State, StatesGroup
-
+# pylint: disable=R0903
 
 class AddInspector(StatesGroup):
     """Класс для описания состояния"""
