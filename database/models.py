72499c67c8e07ad59c750e4c4405653869572672
Nata-alt-ops <nataliaklucnikova9@gmail.com>
2025-05-15 10:05:51 +0300

    handler user.py

--- database/models.py
+++ database/models.py
@@ -1,5 +1,6 @@
 """Модуль для базы данных"""
-from peewee import SqliteDatabase, Model, CharField, IntegerField
+from peewee import SqliteDatabase, Model, CharField, IntegerField, DateField
+from datetime import date
 # pylint: disable=R0903
 DB = SqliteDatabase('sqlite.db')
 
@@ -14,10 +15,11 @@ class Table(Model):
 class User(Table):
     """Класс пользователя"""
     tg_id = IntegerField(unique=True)
+    at_created = DateField(default=date.now())
     username = CharField(null=True)
     last_name = CharField(null=True)
     first_name = CharField(null=True)
-    telephone = IntegerField(null=True)
+    phone = IntegerField(null=True)
 
 
 if __name__ == "__main__":
---
+++ handler/user.py
@@ -0,0 +1,34 @@
+"""Обработка команды start"""
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from database.models import User
+
+router = Router()
+
+@router.massage(CommandStart())
+async def cmd_start(message: Message):
+    """Обработчик команды start"""
+
+    await message.answer(text='Добрый день.' 
+                        'Через этого бота Вы можете отправить анонимное сообщение о пьяном водителе')
+    
+    user = User.get_or_none(tg_id = message.from_user.id)
+    
+    if user in None:
+        User.create(
+            tg_id = message.from_user.id,
+            username = message.from_user.username,
+            last_name = message.from_user,
+            first_name = message.from_user.first_name,
+        )
+    elif (
+        User.username != message.from_user.username or 
+        User.last_name != message.from_user.last_name or
+        User.first_name != message.from_user.first_name
+    ):
+        User.update(
+            username = message.from_user.username,
+            last_name = message.from_user.last_name,
+            first_name = message.from_user.first_name,
+            )
