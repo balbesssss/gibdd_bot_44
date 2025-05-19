c0cae2abaafef1ab63b4eb1232971b48423512d9
Nikita Rumyantsev <nikita.ded0000@gmail.com>
2025-05-19 17:00:35 +0300

    Добавление роли администратора

--- handlers/admin/add_admin.py
+++ handlers/admin/add_admin.py
@@ -4,7 +4,7 @@ from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 from filters.admin import IsAdmin
-from states.admin.admin import AddAdmin
+from database.models import User, UserRole
 
 router = Router()
 
@@ -12,6 +12,37 @@ router = Router()
 @router.message(F.text == "Добавить администратора", IsAdmin())
 async def add_admin(message: Message, state: FSMContext):
     """Обработчик сообщения для добавления администратора"""
+    contact = message.contact
+    user_id = contact.user_id if contact.user_id else None
 
-    await message.answer(text="Отправьте контакт сотрудника")
-    await state.set_state(AddAdmin.get_contact)
+    user = User.get_or_none(User.tg_id == user_id)
+    if not user:
+        await message.answer("Такой сотрудник не запускал бота")
+        await state.clear()
+        return
+
+    if contact.phone_number and user.phone != contact.phone_number:
+        user.phone = contact.phone_number
+        user.save()
+
+    if contact.last_name and user.last_name != contact.last_name:
+        user.last_name = contact.last_name
+        user.save()
+
+    if contact.first_name and user.first_name != contact.first_name:
+        user.first_name = contact.first_name
+        user.save()
+
+    admin_role = IsAdmin.role
+
+    user_role = UserRole.get_or_none(
+        (UserRole.user == user) &
+        (UserRole.role == admin_role)
+    )
+
+    if user_role:
+        await message.answer("Этому сотруднику уже выдавалась роль инспектора")
+    else:
+        UserRole.create(user=user, role=admin_role)
+        await message.answer("Роль Инспектора добавлена")
+    await state.clear()
