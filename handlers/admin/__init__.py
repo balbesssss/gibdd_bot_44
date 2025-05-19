c57dc54d97cb7fcfca7b1641a612ad50899fcc17
Nikita Rumyantsev <nikita.ded0000@gmail.com>
2025-05-19 17:35:49 +0300

    добавление роутеров:? исправлено

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -1,10 +1,9 @@
 """подключение роутеров"""
 
 from aiogram import Dispatcher
-from .add_inspector import router as add_inspector_router
-from .add_admin import router as add_admin_router
+from .add_role import router as add_role_router
 
 
 def add_routers(dp: Dispatcher):
     """Подключение роутеров"""
-    dp.include_routers(add_inspector_router, add_admin_router)
+    dp.include_routers(add_role_router)
