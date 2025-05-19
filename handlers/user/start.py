6a3a05f11b15b6f4aaeb386ab3d29d7d8c2b3154
aleeeGG <vltrv50@gmail.com>
2025-05-19 16:37:39 +0300

    Добавил кнопки для Администратора

--- handlers/user/start.py
+++ handlers/user/start.py
@@ -3,22 +3,16 @@
 from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-from database.models import User
-
+from database.models import User, UserRole
+from filters.admin import IsAdmin
+from keyboards.admin import KB
 router = Router()
 
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     """Обработчик команды start"""
-
-    await message.answer(
-        text="Добрый день.Через этого бота "
-        "Вы можете отправить анонимное сообщение о пьяном водителе"
-    )
-
     user = User.get_or_none(tg_id=message.from_user.id)
-
     if user is None:
         User.create(
             tg_id=message.from_user.id,
@@ -36,3 +30,11 @@ async def cmd_start(message: Message):
         user.last_name = message.from_user.last_name
         user.first_name = message.from_user.first_name
         user.save()
+    is_admin = UserRole.get_or_none(
+        (UserRole.user == user) &
+        (UserRole.role == IsAdmin.role)
+    )
+
+    await message.answer("Добрый день.Через этого бота "
+                         "Вы можете отправить анонимное сообщение о пьяном водителе.\n",
+                         reply_markup=KB if is_admin else None)
---
+++ keyboards/admin.py
@@ -0,0 +1,14 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+KB = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Добавить инспектора"),
+            KeyboardButton(text="Показать инспекторов")
+        ],
+        [
+            KeyboardButton(text="Добавить администратора"),
+            KeyboardButton(text="Показать администраторов")
+        ],
+    ],
+    resize_keyboard=True
+    )
