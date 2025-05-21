1e145eb4bf99bcdfa7dff5de002ed5a56a7fc321
Максим Еперин <maksimeperin@gmail.com>
2025-05-21 15:16:12 +0300

    modified:   handlers/user/send_message.py 	modified:   keyboards/user.py

--- handlers/user/send_message.py
+++ handlers/user/send_message.py
@@ -5,24 +5,24 @@ from aiogram.types import Message
 from database.models import UserRole
 from filters.user import IsUser
 from filters.inspector import IsInspector
-from keyboards.user import MESUSER
+from keyboards.user import MESSAGE_USER
 
 router = Router()
 
 
 @router.message(IsUser())
 async def get_message_from_user(message: Message):
-    """Обработчик сообщения от пользователя"""
 
+    """Обработчик сообщения от пользователя"""
     await message.answer(
         "Спасибо за обращение. Мы его уже передали инспекторам"
     )
-
     user_roles = list(
         UserRole.select().where(UserRole.role == IsInspector().role)
     )
-
     for user_role in user_roles:
         await message.bot.send_message(
-            chat_id=user_role.user.tg_id, text=message.text, reply_markup=MESUSER
+            chat_id=user_role.user.tg_id,
+            text=message.text,
+            reply_markup=MESSAGE_USER
         )
--- keyboards/user.py
+++ keyboards/user.py
@@ -1,8 +1,13 @@
 """Клавиатуры для Администратора"""
 
+
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-MESUSER = InlineKeyboardMarkup(
+
+MESSAGE_USER = InlineKeyboardMarkup(
+
     inline_keyboard=[[
+
         InlineKeyboardButton(text="Бан", callback_data='Ban'),
+
     ]])
