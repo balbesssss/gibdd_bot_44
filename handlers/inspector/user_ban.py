0f61bdb06238cab4ed0f7d6a3d65dbff23b81c6b
YuriSilenok <yuri.silenok@gmail.com>
2025-06-03 11:43:15 +0300

    s

--- handlers/inspector/user_ban.py
+++ handlers/inspector/user_ban.py
@@ -13,14 +13,6 @@ router = Router()
 
 
 @router.callback_query(F.data.startswith("ban_"), IsAdmin())
-async def show_inspect(callback: CallbackQuery):
-    """Подтверждение блокирования пользователя."""
-    user_id = callback.data.split("_")[-1]
-    await callback.message.edit_reply_markup(
-        reply_markup=user_ban_cobfirm_and_cancel_kb(user_id)
-    )
-
-
 @router.callback_query(F.data.startswith("ban_"), IsInspector())
 async def show_inspectors(callback: CallbackQuery):
     """Подтверждение блокирования пользователя."""
